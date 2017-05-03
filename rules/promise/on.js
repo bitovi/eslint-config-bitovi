a102ac06c94aafd25e54596238285c65a02cebe6
imjoshdean <imjoshdean@me.com>
2017-05-03 16:47:29 -0600

    🛠 Fix missing promise/ in rules

--- rules/promise/on.js
+++ rules/promise/on.js
@@ -4,26 +4,26 @@ module.exports = {
   plugins: ['promise'],
   rules: {
     // enforces the use of catch on un-returned promises.
-    'catch-or-return': 0,
+    'promise/catch-or-return': 'error',
     // avoid wrapping values in Promise.resolve or Promise.reject when not needed.
-    'no-return-wrap': 0,
+    'promise/no-return-wrap': 'error',
     // enforce consistent param names when creating new promises.
-    'param-names': 0,
+    'promise/param-names': 'error',
     // return inside each then to create readable and reusable Promise chains.
-    'always-return': 0,
+    'promise/always-return': 'error',
     // In an ES5 environment, make sure to create a Promise constructor before using.
-    'no-native': 0,
+    'promise/no-native': 0,
     // avoid nested .then() or .catch() statements
-    'no-nesting': 0,
+    'promise/no-nesting': 0,
     // avoid using promises inside of callbacks
-    'no-promise-in-callback': 0,
+    'promise/no-promise-in-callback': 0,
     // avoid calling cb() inside of a then() (use nodeify] instead)
-    'no-callback-in-promise': 0,
+    'promise/no-callback-in-promise': 0,
     // avoid creating new promises outside of utility libs (use pify instead)
-    'avoid-new': 0,
+    'promise/avoid-new': 0,
     // prefer await to then() for reading Promise values
-    'prefer-await-to-then': 0,
+    'promise/prefer-await-to-then': 0,
     // prefer async/await to the callback pattern
-    'prefer-await-to-callbacks': 0
+    'promise/prefer-await-to-callbacks': 0
   }
 };
